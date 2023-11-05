49c7a84e54c78fd54694e2f9075c92a9408bc772
Sourav Nandi <49252786+Sourav333@users.noreply.github.com>
2023-11-05 18:38:16 +0530

    first commit

---
+++ src/reduxContainer/BookContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import purchase_book from './bookAction';
+
+function BookContainer() {
+    const noOfBooks = useSelector(state => state.NumberofBooks)
+    const dispatch = useDispatch()
+  return (
+    <>
+    <div>BookContainer</div>
+    <h2>No of Books - {noOfBooks}</h2>
+    <button onClick={()=>(dispatch(purchase_book()))}>Buy Book</button>
+    </>
+  )
+}
+
+export default BookContainer
---
+++ src/reduxContainer/bookAction.js
@@ -0,0 +1,6 @@
+import { buy_book } from "./BookTypes";
+const purchase_book = () => {
+    return{
+        type: buy_book
+    }
+}
